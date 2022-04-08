337797b5806ad2c538a6f9c671e71a6a9278e15b
Emmanuel6093 <elorenzo6093@gmail.com>
2022-04-08 17:58:20 -0400

    html page template complete

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -134,18 +138,17 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+         if (err) throw err;
+
+         console.log('Portfolio complete! Check out index.html to see the output!');
+     });
 });
 
-// const fs = require('fs');
 
-// const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
